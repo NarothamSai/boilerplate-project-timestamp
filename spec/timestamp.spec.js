c79fb10bb58d096a74333b74b4d9291a9ea32296
narothamsai <>
2021-07-08 12:05:44 +0000

    Add test for get current timestamp

---
+++ spec/timestamp.spec.js
@@ -0,0 +1,22 @@
+const {getCurrentTimestamp} = require("../timestamp.js");
+
+describe("Timestamp test suite",function(){
+
+  describe("Expectations for Get current timestamp",function(){
+    let result = getCurrentTimestamp();
+
+    it("function should return JSON object",function(){
+      expect(result).not.toBe(null);
+      expect(typeof result).toBe("object");
+    })
+
+    it("function should return JSON object with unix key",function(){
+      expect(result.hasOwnProperty('unix')).toBe(true);
+    })
+
+    it("function should return JSON object with utc key",function(){
+      expect(result.hasOwnProperty('utc')).toBe(true);
+    })
+  })
+
+})

416d69fb6ffe9639141d799f5bc3dc4039791049
narothamsai <unknown>
2021-07-09 12:57:32 +0000

    Add getFrom Date

--- timestamp.js
+++ timestamp.js
@@ -1,5 +1,4 @@
-function Timestamp(date){
-  this.date = date;
+function Timestamp(){
 }
 
 Timestamp.prototype.getCurrent = function(){
@@ -10,6 +9,33 @@ Timestamp.prototype.getCurrent = function(){
   timestamp.utc = date.toUTCString();
 
   return timestamp;
+}
+
+Timestamp.prototype.getFromDate = function(date){
+  let timestamp = {};
+
+  if(this.isNotANumber(date) == false){
+    date = parseInt(date);
+  }
+
+  date = new Date(date);
+
+  if(this.isInvalidDate(date) == true){
+    timestamp.error = "Invalid Date"
+  }else{
+    timestamp.unix = date.valueOf();
+    timestamp.utc = date.toUTCString();
+  }
+
+  return timestamp;
+}
+
+Timestamp.prototype.isNotANumber = function(date){
+  return isNaN(date);
+}
+
+Timestamp.prototype.isInvalidDate = function(date){
+  return date == "Invalid Date";
 }
 
 module.exports = Timestamp;
